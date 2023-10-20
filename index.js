eca5e9b276dc56ab6e49c7bb934316599bd692da
MWANZIA <shem.masoo@student.moringaschool.com>
2023-10-20 17:40:04 +0300

    project

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
-const renderPage = function(){
+const resultsDiv = document.createElement('div');
+resultsDiv.id = 'results_div';
+document.body.appendChild(resultsDiv);
 
-    const resultsDiv= document.createElement('div')
-    resultsDiv.id ='results_div'
-    document.body.appendChild(resultsDiv)
+const word = document.querySelector('#search_for_word');
+const form = document.querySelector('#form');
 
-    const word = document.querySelector('#search_for_word')
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
+    .then(response => response.json())
+    .then(data => {
+        
+        //console.log(data);
+        //console.log(data[0].meanings[0].definitions[0].definition);
+        const definitions = data[0].meanings[0].definitions;
+     
+        //console.log(definitions);
+        resultsDiv.textContent = '';
 
-    const form =document.querySelector('#form');
-
-    const renderPage = function(){
-
-        const resultsDiv= document.createElement('div')
-        resultsDiv.id ='results_div'
-        document.body.appendChild(resultsDiv)
-          console.log(resultsDiv)
-    
-        const word = document.querySelector('#search_for_word')
-           console.log(word);
-    
-        const form =document.querySelector('#form');
-    
-        form.addEventListener("submit",(e) =>
-        {
-            e.preventDefault()
-            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${internet}`)
-            .then(Response=> Response.json())
-            .then(data =>{
-                console.log(data)
-                console.log(data[0].meanings[0].definitions[0].definitions)
-    
-                const definitions =data[0].meanings[0].definitions
-                 console.log(definitions)
-                 resultsDiv.textContent='';
-    
-                 definitions.forEach(element =>{
-                    const results =document.createElement('li')
-                    results.textContent=element.definitions
-                    resultsDiv.appendChild(results)
-                 })
-                 word.value =''
-            })
+        definitions.forEach(element => {
+            const results = document.createElement('li');
+            results.textContent = element.definition;
+            resultsDiv.appendChild(results);
         })
-    }
-}
+        word.value = '';
+    
+    });
+    })
