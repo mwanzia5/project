68b6770374d0e97c8678ebc0e2142b032a8d8aad
MWANZIA <shem.masoo@student.moringaschool.com>
2023-10-20 17:50:44 +0300

    project

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const resultsDiv = document.createElement('div');
-resultsDiv.id = 'results_div';
-document.body.appendChild(resultsDiv);
+const renderPage = function(){
 
-const word = document.querySelector('#search_for_word');
-const form = document.querySelector('#form');
+    const resultsDiv = document.createElement('div');
+    resultsDiv.id = 'results_div';
+    document.body.appendChild(resultsDiv);
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
-    .then(response => response.json())
-    .then(data => {
+    const word = document.querySelector('#search_for_word');
+    const form = document.querySelector('#form');
+
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
+        .then(response => response.json())
+        .then(data => {
+            
+            //console.log(data);
+            //console.log(data[0].meanings[0].definitions[0].definition);
+            const definitions = data[0].meanings[0].definitions;
+         
+            //console.log(definitions);
+            resultsDiv.textContent = '';
+
+            definitions.forEach(element => {
+                const results = document.createElement('li');
+                results.textContent = element.definition;
+                resultsDiv.appendChild(results);
+            })
+            word.value = '';
         
-        //console.log(data);
-        //console.log(data[0].meanings[0].definitions[0].definition);
-        const definitions = data[0].meanings[0].definitions;
-     
-        //console.log(definitions);
-        resultsDiv.textContent = '';
-
-        definitions.forEach(element => {
-            const results = document.createElement('li');
-            results.textContent = element.definition;
-            resultsDiv.appendChild(results);
+        });
         })
-        word.value = '';
-    
-    });
-    })
+const about = document.querySelector('#about_button');
+about.addEventListener('click',() => {
+    resultsDiv.textContent = 'This site uses the Free Dictionary API. Type your word in the search box and discover the meaning.'
+})
+}
+document.addEventListener('DOMContentLoaded',renderPage);
