00ddfcac6d9193406542cab75012dc07095fcbd5
MWANZIA <shem.masoo@student.moringaschool.com>
2023-10-20 10:12:09 +0300

    project

--- index.js
+++ index.js
@@ -8,23 +8,36 @@ const renderPage = function(){
 
     const form =document.querySelector('#form');
 
-    form.addEventListener("submit",(e) =>
-    {
-        e.preventDefault()
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}')
-        .then(Response=> Response.json())
-        .then(data =>{
-            console.log(data)
-            console.log(data[0].meanings[0].definitions[0].definitions)
+    const renderPage = function(){
 
-            const definitions =data[0].meanings[0].definitions
-             console.log(definitions)
-             resultsDiv.textContent='';
-
-             definitions.forEach(element =>{
-                const results =document.createElement('li')
-                results.textContent=''
-             })
+        const resultsDiv= document.createElement('div')
+        resultsDiv.id ='results_div'
+        document.body.appendChild(resultsDiv)
+    
+        const word = document.querySelector('#search_for_word')
+    
+        const form =document.querySelector('#form');
+    
+        form.addEventListener("submit",(e) =>
+        {
+            e.preventDefault()
+            fetch('https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}')
+            .then(Response=> Response.json())
+            .then(data =>{
+                console.log(data)
+                console.log(data[0].meanings[0].definitions[0].definitions)
+    
+                const definitions =data[0].meanings[0].definitions
+                 console.log(definitions)
+                 resultsDiv.textContent='';
+    
+                 definitions.forEach(element =>{
+                    const results =document.createElement('li')
+                    results.textContent=element.definitions
+                    resultsDiv.appendChild(results)
+                 })
+                 word.value =''
+            })
         })
-    })
+    }
 }
